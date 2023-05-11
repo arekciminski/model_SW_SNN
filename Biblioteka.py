e44d6841cc5301a2cb39f0d6b93e30a9d56840f5
arekciminski <arek.ciminski@gmail.com>
2023-05-11 11:41:34 +0200

    Działająca biblioteka pomocnicza

--- Biblioteka.py
+++ Biblioteka.py
@@ -1,74 +1,130 @@
 from epanettools import epanet2 as et
 class Epa:
 
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self, parameter):
+        self.parameter = parameter
         self.patern_index = []
         self.link_index = []
         self.patern_index = []
         self.nnodes = []
         self.nlinks = []
+
     def open_epanet(self):
-        ret = et.ENopen(self.file_name, "Net3.rpt", "")
+        ret = et.ENopen(self.parameter['file_name'], "Net3.rpt", "")
+
     def close_epanet(self):
         ret = et.ENclose()
+
     def get_number_of_nodes(self):
         ret, nnodes = et.ENgetcount(et.EN_NODECOUNT)
         self.nnodes = nnodes
         return nnodes
+
     def get_number_of_links(self):
         ret, nlinks = et.ENgetcount(et.EN_LINKCOUNT)
         self.nlinks = nlinks
         return nlinks
-    def get_link_number(self,link_names):
+
+    def get_link_index(self):
         link_index=[]
-        for ln in link_names:
+        for ln in self.parameter['mes_link_name']:
             ret, lx = et.ENgetlinkindex(ln)
             link_index.append(lx)
         self.link_index = link_index
-        return link_index
-    def get_node_number(self,node_names):
+
+    def get_node_index(self):
         node_index=[]
-        for nn in node_names:
+        for nn in self.parameter['mes_node_name']:
             ret, nx = et.ENgetnodeindex(nn)
             node_index.append(nx)
-        self.node_index = node_index
-        return node_index
-    def get_patern_number(self,patern_names):
-        patern_index=[]
-        for pn in patern_names:
+            self.node_index = node_index
+        tank_index=[]
+        for nn in self.parameter['tank_names']:
+            ret, nx = et.ENgetnodeindex(nn)
+            tank_index.append(nx)
+            self.tank_index = tank_index
+
+    def get_pattern_index(self):
+        pattern_index=[]
+        for pn in self.parameter['pattern_names']:
             ret, px = et.ENgetpatternindex(pn)
-            patern_index.append(px)
-        self.patern_index = patern_index
-        return patern_index
-    def get_hydraulic_values(self,hydrailic_values):
+            pattern_index.append(px)
+        print(pattern_index)
+        self.pattern_index = pattern_index
+
+    def set_time_duration(self):
+        et.ENsettimeparam(0,self.parameter['time_duration'])
+
+    def set_tank_inital(self):
+        for tank_in,initial_val in zip(self.tank_index,self.parameter['initial_tanks']):
+            et.ENsetnodevalue(tank_in,8,initial_val)
+
+    def set_patern_values(self):
+         for i in range(len(self.pattern_index)):
+            for j in range(len(self.parameter['pattern_values'][0])):
+                et.ENsetpatternvalue(self.pattern_index[i],j+1,self.parameter['pattern_values'][i][j])
+
+    def save_temp_file(self):
+        et.ENsaveinpfile('temporary_'+self.parameter['file_name'])
+
+    def get_set_parameters(self):
+        ep.get_link_index()
+        ep.get_node_index()
+        ep.get_pattern_index()
+        ep.set_time_duration()
+        ep.set_tank_inital()
+        #ep.set_patern_values()
+        #ep.save_temp_file()
+
+    def get_hydraulic_values(self):
+
         et.ENopenH()
         et.ENinitH(0)
         time=[]
-        flow=[[]]*len(self.link_index)
-        pressure=[[]]*len(self.node_index)
-        head=[[]]*len(self.node_index)
+        flow=[[] for i in range(len(self.link_index))]
+        pressure=[[] for i in range(len(self.node_index))]
+        head=[[] for i in range(len(self.node_index))]
         error = []
         while True:
             ret, t = et.ENrunH()
             error.append(ret)
             time.append(t)
-            if hydrailic_values[0] == 'flow':
+            if self.parameter['hydraulic_values'][0] == 'flow':
                 for i in range(0,len(self.link_index)):
                     #print(self.link_index[i])
                     ret, p = et.ENgetlinkvalue(self.link_index[i], et.EN_FLOW)
                     flow[i].append(p)
-                    print(self.link_index[i],p, flow[i])
-                #print(i,p,len(flow),len(flow[0]),t/3600)
             for i in range(0, len(self.node_index)):
-                if hydrailic_values[1] == 'pressure':
-                    ret, p = et.ENgetnodevalue(i, et.EN_PRESSURE)
+                if self.parameter['hydraulic_values'][1] == 'pressure':
+                    ret, p = et.ENgetnodevalue(self.node_index[i], et.EN_PRESSURE)
                     pressure[i].append(p)
-                if hydrailic_values[2] == 'head':
-                    ret, p = et.ENgetnodevalue(i, et.EN_HEAD)
+                if self.parameter['hydraulic_values'][2] == 'head':
+                    ret, p = et.ENgetnodevalue(self.node_index[i], et.EN_HEAD)
                     head[i].append(p)
             ret, tstep = et.ENnextH()
             if (tstep <= 0):
                 break
         ret = et.ENcloseH()
         return flow, pressure, head, error, time
+
+if __name__ == '__main__':
+    parameters = {
+    'file_name' : "siec_maly_obiekt.inp",
+    'mes_link_name' :['1','2'],
+    'mes_node_name':['7'],
+    'pumps_names' : ['pompka'],
+    'pattern_names' :['demand','pompka'],
+    'pattern_values' : [[1],[0.4]],
+    'time_duration': 3600,
+    'tank_names': ['7'],
+    'initial_tanks' : [4],
+    'hydraulic_values': ['flow', 'pressure', 'head']
+    }
+
+    ep = Epa(parameters)
+    ep.open_epanet()
+    ep.get_set_parameters()
+    ep.set_patern_values()
+    [flow, pressure, head, error, time] = ep.get_hydraulic_values()
+    ep.close_epanet()
+    print(pressure)
--- main.py
+++ main.py
@@ -9,22 +9,28 @@ print([[0]]*5)
 file_name = "chojnice_kwiecien_obiekt.inp"
 file_name = "siec_maly_obiekt.inp"
 
-mes_link_name =['1','2']
-mes_node_name =['1','2']
-#pumps_names = ['F1','K1','P1']
-pat_name =['demand','pompka']
+parameters = {
+    'file_name' : "siec_maly_obiekt.inp",
+    'mes_link_name' :['1','2'],
+    'mes_node_name':['1','2','7'],
+    'pumps_names' : ['pompka'],
+    'pattern_names' :['demand','pompka'],
+    'pattern_values' : [[0.3],[0.7331]],
+    'time_duration': 3600,
+    'tank_names': ['7'],
+    'initial_tanks' : [2],
+    'hydrailic_values' :['flow', 'pressure', 'head']}
+
+
 ep = Epa(file_name)
 ep.open_epanet()
-print(ep.get_number_of_links())
-print(ep.get_number_of_nodes())
-print(ep.get_link_number(mes_link_name))
 #print(ep.get_link_number(pumps_names))
-print(ep.get_node_number(mes_node_name))
-print(ep.get_patern_number(pat_name))
+(ep.get_node_number(mes_node_name))
+(ep.get_patern_number(pat_name))
 [flow,pressure,head,error,time]=ep.get_hydraulic_values(['flow','pressure','head'])
 ep.close_epanet()
 
-print(len(flow[0]),len(error)*3)
+print(error)
 
 
 
