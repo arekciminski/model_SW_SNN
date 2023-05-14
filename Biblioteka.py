6e186f2006f67ac6ef61e35241ee39d9a038457a
arekciminski <arek.ciminski@gmail.com>
2023-05-14 16:31:24 +0200

    Wprowadzono porawki do symulowania na 24 h

--- Biblioteka.py
+++ Biblioteka.py
@@ -1,6 +1,7 @@
 from epanettools import epanet2 as et
 from tqdm import tqdm
 import numpy as np
+import random
 
 class Epa:
 
@@ -52,33 +53,34 @@ class Epa:
 
     def get_pattern_index(self):
         pattern_index=[]
-        for pn in self.parameters['pumps_patterns']:
+        for pn in self.parameters['pumps_patterns_names']:
             ret, px = et.ENgetpatternindex(pn)
             pattern_index.append(px)
         self.pumps_pattern_index = pattern_index
 
         pattern_index=[]
-        for pn in self.parameters['demand_patterns']:
+        for pn in self.parameters['demand_patterns_names']:
             ret, px = et.ENgetpatternindex(pn)
             pattern_index.append(px)
         self.demand_pattern_index = pattern_index
 
 
     def set_time_duration(self):
-        et.ENsettimeparam(0,self.parameters['time_duration'])
+        et.ENsettimeparam(0,self.parameters['time_duration_s'])
 
     def set_tank_inital(self):
-        for tank_in,initial_val in zip(self.tank_index,self.parameters['initial_tanks']):
-            et.ENsetnodevalue(tank_in,8,initial_val)
+        for tank_in,initial_val in zip(self.tank_index,self.random['tanks_init_val']):
+            et.ENsetnodevalue(tank_in,8,initial_val[0])
 
     def set_patern_values(self):
-         for i in range(len(self.demand_pattern_index)):
-            for j in range(len(self.parameters['demand_pattern_values'][0])):
-                et.ENsetpatternvalue(self.demand_pattern_index[i],j+1,self.parameters['demand_pattern_values'][i][j])
-
          for i in range(self.parameters['num_pumps']):
-            for j in range(len(self.parameters['pumps_pattern_values'][0])):
-                et.ENsetpatternvalue(self.pumps_pattern_index[i],j+1,self.parameters['pumps_pattern_values'][i][j])
+            #print(self.data.keys())
+            for j in range(self.parameters['time_duration_h']):
+                et.ENsetpatternvalue(self.pumps_pattern_index[i],j+1,self.random['pump_input'][i][j])
+
+         for i in range(self.parameters['num_demands']):
+            for j in range(self.parameters['time_duration_h']):
+                et.ENsetpatternvalue(self.demand_pattern_index[i],j+1,self.random['demand_val'][i][j])
 
 
     def save_temp_file(self):
@@ -89,10 +91,12 @@ class Epa:
         self.get_node_index()
         self.get_pattern_index()
         self.set_time_duration()
-        #ep.save_temp_file()
 
     def prepare_empty_dict_to_comput(self):
+
         self.data = {'error_output': []}
+
+        self.data['time_output'] = []
 
         for mes_node in self.parameters['mes_nodes_names']:
             self.data['head_output_' + mes_node] = []
@@ -112,7 +116,7 @@ class Epa:
         for pump_input in self.parameters['pumps_names']:
             self.data['pump_input_' + pump_input] = []
 
-        for demand_input in self.parameters['demand_patterns']:
+        for demand_input in self.parameters['demand_patterns_names']:
             self.data['demand_input_' + demand_input] = []
 
     def get_hydraulic_values(self):
@@ -153,51 +157,58 @@ class Epa:
 
     def generate_random(self):
 
-        pump_control = []
-        for ii in range(0, self.parameters['num_pumps'] + 1):
-            pump_control.append([np.random.randint(50, 100) / 100])
-        self.random['pump_control'] = pump_control
+        self.random['pump_input'] = []
+        self.random['demand_val'] = []
+        self.random['tanks_init_val'] = []
 
-        demand_val = []
-        for ii in range(0, self.parameters['num_demands'] + 1):
-            demand_val.append([np.random.randint(1, 100) / 100])
-        self.random['demand_val'] = demand_val
+        for ii in range(0, self.parameters['num_pumps']):
+            pump_control = []
+            for i in range(1, self.parameters['time_duration_h'] + 1):
+                pump_control.append(random.randint(50, 100)/100)
+            self.random['pump_input'].append(pump_control)
+            self.data['pump_input_' + self.parameters['pumps_names'][ii]].append(pump_control)
 
-        tanks_init_val = []
+        for ii in range(0, self.parameters['num_demands']):
+            demand_val = []
+            for i in range(1, self.parameters['time_duration_h'] + 1):
+                demand_val.append(random.randint(1, 100) / 100)
+            self.random['demand_val'].append(demand_val)
+            self.data['demand_input_' + self.parameters['demand_patterns_names'][ii]].append(demand_val)
+
         for ii in range(0, self.parameters['num_tanks']):
-            tanks_init_val.append(np.random.randint(self.parameters['min_tanks_lev'][ii] * 10,
-                                                    self.parameters['max_tanks_lev'][ii] * 10) / 10)
-        self.random['tanks_init_val'] = tanks_init_val
+            tanks_init_val = []
+            tanks_init_val.append(random.randint(self.parameters['min_tanks_lev'][ii] * 10,
+                                                 self.parameters['max_tanks_lev'][ii] * 10) / 10)
+            self.random['tanks_init_val'].append(tanks_init_val)
+            self.data['tank_input_' + self.parameters['tanks_names'][ii]].append(tanks_init_val)
 
-        for ii in range(self.parameters['num_demands']):
-            self.data['demand_input_' + self.parameters['demand_patterns'][ii]].append(demand_val[ii][0])
+        #print(demand_val.flatten())
 
-        for ii in range(self.parameters['num_pumps']):
-            self.data['pump_input_' + self.parameters['pumps_names'][ii]].append(pump_control[ii][0])
+    def insert_data(self, head,flow, energy, error, time):
 
-        for ii in range(self.parameters['num_tanks']):
-            self.data['tank_input_' + self.parameters['tanks_names'][ii]].append(tanks_init_val[ii])
-
-        self.parameters['initial_tanks'] = tanks_init_val
-
-        self.parameters['demand_pattern_values'] = demand_val
-        self.parameters['pumps_pattern_values'] = pump_control
-
-    def insert_data(self, head,flow, energy, error):
         for ii in range(0, self.parameters['num_mes_nodes'] - self.parameters['num_tanks'] + 1):
-            self.data['head_output_' + self.parameters['mes_nodes_names'][ii]].append(head[ii][0])
+            self.data['head_output_' + self.parameters['mes_nodes_names'][ii]].append(head[ii])
 
         for ii in range(self.parameters['num_mes_nodes'] - self.parameters['num_tanks'],
                         self.parameters['num_mes_nodes']):
             self.data['tank_output_' + self.parameters['tanks_names'][
-                self.parameters['num_mes_nodes'] - ii - 1]].append(head[ii][0])
+                self.parameters['num_mes_nodes'] - ii - 1]].append(head[ii])
 
         for ii in range(self.parameters['num_mes_links']):
-            self.data['flow_output_' + self.parameters['mes_links_names'][ii]].append(flow[ii][0])
+            self.data['flow_output_' + self.parameters['mes_links_names'][ii]].append(flow[ii])
 
         for ii in range(self.parameters['num_pumps']):
-            self.data['energy_output_' + self.parameters['pumps_names'][ii]].append(energy[ii][0])
-        self.data['error_output'].append(error[0])
+            self.data['energy_output_' + self.parameters['pumps_names'][ii]].append(energy[ii])
+
+        self.data['time_output'].append(time)
+
+        self.data['error_output'].append(error)
+
+    def proper_data(self,flow, energy, head, error, time):
+        print(time%self.parameters['hydraulic_step_s'])
+
+
+
     def get_data(self):
 
         self.prepare_empty_dict_to_comput()
@@ -205,16 +216,18 @@ class Epa:
         self.open_epanet()
         self.get_set_parameters()
 
-        for i in tqdm(range(1, self.parameters['number_iteration'])):
+        for i in tqdm(range(1, self.parameters['number_iteration']+1)):
             #print(i)
             self.generate_random()
             self.set_tank_inital()
             self.set_patern_values()
+            #self.save_temp_file()
 
             [flow, energy, head, error, time] = self.get_hydraulic_values()
 
-            self.insert_data(head , flow , energy , error)
+            [flow, energy, head, error, time] = self.proper_data(flow, energy, head, error, time)
 
+            self.insert_data(head , flow , energy , error,time)
 
         self.close_epanet()
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 from Biblioteka import Epa
 from multiprocessing import Process
 import pandas as pd
-import time
+import datetime
+
 
 file_name = "chojnice_kwiecien_obiekt.inp"
 
@@ -12,47 +13,54 @@ parameters = {
     'mes_links_names': ['F1','K1','P1'],
     'mes_nodes_names': ['083','158','180'],
     'pumps_names': ['F1','K1','P1'],
-    'pumps_patterns': ['f1','k1','p1'],
-    'demand_patterns': ['par2','kar2'],
-    'pumps_pattern_values': [[0.8],[0.8],[0.8]],
-    'demand_pattern_values': [[0.7],[0.7],[0.7]],
-    'time_duration': 3600,
+    'pumps_patterns_names': ['f1','k1','p1'],
+    'demand_patterns_names': ['par2','kar2'],
+    'time_duration_h': 24,  # [h]
+    'hydraulic_step_s': 3600,  # [h]
     'tanks_names': ['180'],
     'initial_tanks_lev': [3.58],
-    'min_tanks_lev':[0.5],
+    'min_tanks_lev':[1.2],
     'max_tanks_lev':[5],
     'hydraulic_values': ['flow', 'energy', 'head'],
-    'number_iteration':1_000_000
+    'number_iteration':1
 }
 
 parameters['num_mes_links'] = len(parameters['mes_links_names'])
 parameters['num_mes_nodes'] = len(parameters['mes_nodes_names'])
 parameters['num_pumps'] = len(parameters['pumps_names'])
-parameters['num_demands'] = len(parameters['demand_patterns'])
+parameters['num_demands'] = len(parameters['demand_patterns_names'])
 parameters['num_tanks'] = len(parameters['tanks_names'])
-
+parameters['time_duration_s'] = parameters['time_duration_h']*3600#[s]
 
 ep = Epa(parameters)
 
-t = time.process_time()
+t = datetime.datetime.now()
 ep.get_data()
-print(time.process_time() - t)
+#print(datetime.datetime.now()-t)
 
-'''t = time.process_time()
-p = Process(target = ep.get_data())
-p.start()
-p.join()
-print(time.process_time() - t)'''
+keis = ['tank_output_180','tank_input_180', 'pump_input_F1', 'pump_input_K1', 'pump_input_P1', 'demand_input_par2', 'demand_input_kar2']
 
 
-'''#print(ep.data.keys())
+'''for ke in keis:
+    print(ke,ep.data[ke])'''
+
+
 for key in ep.data.keys():
-    print(key, ep.data[key])'''
+    temp=[]
+   # try:
+    if type(ep.data[key][0]) != list:
+        temp = ep.data[key]
+    else:
+        for i in range(parameters['number_iteration']):
+            temp += ep.data[key][i]
+    ep.data[key] = temp
+    print(len(ep.data[key]))
+
+#for key in ep.data.keys():
+#    print(key,ep.data[key])
 
 
-df = pd.DataFrame(ep.data)
+'''df = pd.DataFrame(ep.data)
 df = df.reset_index(drop = True)
 df.to_csv('dane_do_uczenia_'+str(parameters['number_iteration'])+'.csv',decimal =',',sep=';')
-#print(df)
-
-
+'''
